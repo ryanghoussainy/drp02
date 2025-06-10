99624d3575f8fb7e262688f626340f0ab939fcc1
Alexander Chow <akc123@ic.ac.uk>
2025-06-10 13:13:18 +0100

    [ac906] feat: Added new map locations for 3 new games

--- team-up-london/constants/maps.ts
+++ team-up-london/constants/maps.ts
@@ -11,4 +11,16 @@ export const GAME_LOCATION: Record<string, { latitude: number; longitude: number
     latitude: 51.500361, 
     longitude: -0.173381,
   },
+  "0917e3d5-5360-40a9-8276-e72576009c7d": {
+    latitude: 51.506249,
+    longitude: -0.176205,
+  },
+  "0eb70567-d514-4f35-8e7c-f2bdcce37466": {
+    latitude: 51.532181,
+    longitude: -0.156138,
+  },
+  "7d92bc03-64da-4aa3-be60-89d808d608f0": {
+    latitude: 51.420048, 
+    longitude: -0.192927
+  },
 };
