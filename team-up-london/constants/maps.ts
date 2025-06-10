b6a80efd044af26f7cdd81fce990afcad5ea9af6
Alexander Chow <akc123@ic.ac.uk>
2025-06-10 12:35:03 +0100

    [ac906] feat: Added new map location

--- team-up-london/constants/maps.ts
+++ team-up-london/constants/maps.ts
@@ -6,5 +6,9 @@ export const GAME_LOCATION: Record<string, { latitude: number; longitude: number
   "7e37465d-b642-4c09-be69-eaf7031c5485": {
     latitude: 51.532181,
     longitude: -0.156138,
-  }
+  },
+  "665a45e4-5f97-45d7-adab-ab63ba95450c": {
+    latitude: 51.500361, 
+    longitude: -0.173381,
+  },
 };
