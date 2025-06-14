6431ec0392c1eeb5a5b59e0f4b34c12091a2216d
Ryan Ghoussainy <ryan@ghoussainy.com>
2025-06-14 23:25:15 +0100

    [rg] maybe

--- team-up-london/components/Push.tsx
+++ team-up-london/components/Push.tsx
@@ -1,31 +1,101 @@
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
 import { supabase } from '../lib/supabase';
 
-
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
     shouldShowBanner: true,
     shouldShowList: true,
   }),
 });
 
+export default function Push({ playerId }: { playerId: string }) {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([]);
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+    undefined
+  );
 
-function handleRegistrationError(errorMessage:  string) {
-  alert(errorMessage);
-  throw new Error(errorMessage);
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(async token => {
+      token && setExpoPushToken(token);
+
+      await supabase
+        .from("players")
+        .update({ expo_push_token: token })
+        .eq("id", playerId);
+    });
+
+    if (Platform.OS === 'android') {
+      Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
+    }
+    const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <Text>{`Channels: ${JSON.stringify(
+        channels.map((c: Notifications.NotificationChannel) => c.id),
+        null,
+        2
+      )}`}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
 }
 
-async function registerForPushNotificationsAsync(playerId: string) {
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here', test: { test1: 'more data' } },
+    },
+    trigger: {
+      type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+      seconds: 2,
+    },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
   if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
+    await Notifications.setNotificationChannelAsync('myNotificationChannel', {
+      name: 'A channel is needed for the permissions prompt to appear',
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
       lightColor: '#FF231F7C',
@@ -40,39 +110,30 @@ async function registerForPushNotificationsAsync(playerId: string) {
       finalStatus = status;
     }
     if (finalStatus !== 'granted') {
-      handleRegistrationError('Permission not granted to get push token for push notification!');
+      alert('Failed to get push token for push notification!');
       return;
     }
-    const projectId =
-      Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-    if (!projectId) {
-      handleRegistrationError('Project ID not found');
-    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
     try {
-      const pushTokenString = (
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error('Project ID not found');
+      }
+      token = (
         await Notifications.getExpoPushTokenAsync({
           projectId,
         })
       ).data;
-
-      await supabase
-        .from('players')
-        .update({ expo_push_token: pushTokenString })
-        .eq('id', playerId);
-        
+      console.log(token);
     } catch (e) {
-      handleRegistrationError(`${e}`);
+      token = `${e}`;
     }
   } else {
-    handleRegistrationError('Must use physical device for push notifications');
+    alert('Must use physical device for Push Notifications');
   }
-}
 
-export default function Push({ playerId }:  { playerId: string }) {
-  useEffect(() => {
-    registerForPushNotificationsAsync(playerId)
-      .catch((error) => console.error(error));
-  }, [playerId]);
-
-  return null;
+  return token;
 }
